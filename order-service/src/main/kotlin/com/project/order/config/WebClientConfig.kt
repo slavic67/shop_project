167d8bcc0154c8ee5d84e4e18d1944663a0701f2
slava <tkac05625@gail.com>
2026-09-06 22:59:55 +0300

    - add: WebClient конфигурация

---
+++ order-service/src/main/kotlin/com/project/order/config/WebClientConfig.kt
@@ -0,0 +1,24 @@
+package com.project.order.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+
+@Configuration
+class WebClientConfig {
+
+    @Value("\${url.notification-service}")
+    private val url: String? = null
+
+    @Bean
+    fun webClientBuilder(): WebClient.Builder {
+        return WebClient.builder()
+    }
+
+    @Bean
+    fun notificationWebClient(builder: WebClient.Builder): WebClient {
+        return builder.baseUrl(url!!).build()
+    }
+}

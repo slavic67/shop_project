73730c89d90941b5762b321b0bbf56ff5d2efaa8
slava <tkac05625@gail.com>
2026-09-12 14:39:29 +0300

    add: настроил logback-spring в сервисе уведомлений

--- notification-service/src/main/java/com/notification/NotificationController.kt
+++ notification-service/src/main/java/com/notification/NotificationController.kt
@@ -1,12 +1,11 @@
 package com.notification
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.lang.Thread.sleep
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -18,7 +17,20 @@ class NotificationController {
     fun notify(@RequestBody request: NotificationRequest): Unit {
         log.info("Отправка сообщения по номеру заказа ${request.orderId}, тип ${request.eventType}")
 
-        sleep(5000)
+        //TODO имитация проблемы, потом удалить
+        val random: Int = Random.nextInt(100)
+
+        log.info("Выпало число: $random")
+
+        if (random < 10) {
+            log.error("Возникли проблемы с отправкой уведомления по orderId: ${request.orderId}")
+            throw RuntimeException("Ошибка отправки сообщения")
+        }
+
+        if (random > 90) {
+            log.info("Сервис замедлился")
+            Thread.sleep(600)
+        }
 
         return Unit
     }
